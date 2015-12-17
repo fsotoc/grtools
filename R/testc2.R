3c74e64c2f1d99e5680f0fe836dab5a39f160ddb
fsotoc <fsoto.c@gmail.com>
2015-12-17 16:18:47 -0500

    replaces proportions equal to 0 or 1 by close approximations to avoid Inf values

--- R/testc2.R
+++ R/testc2.R
@@ -1,15 +1,13 @@
 testc2 <- function(fa1, fa2, n1_n, n2_n){
   
   # replace 0 and 1 by close approximations to avoid Inf values
-  props = c(h1, fa1, h2, fa2)
+  props = c(fa1, fa2)
   if (any(props==0)){warning("Proportions in the data equal to 0; replaced by 1e-10")}
   props[props==0] <- 1e-10
   if (any(props==1)){warning("Proportions in the data equal to 1; replaced by 1-(1e-10)")}
   props[props==1] <- 1-1e-10
-  h1 <- props[1]
-  fa1 <- props[2]
-  h2 <- props[3]
-  fa2 <- props[4]
+  fa1 <- props[1]
+  fa2 <- props[2]
   
   # compute c1 and c2
   c1 <- qnorm(fa1)
